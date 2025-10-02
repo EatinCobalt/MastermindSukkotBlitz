76074b90d1636fc828a8aed565315447c119d054
EatinCobalt <eatincobalt@mail.ru>
2025-10-02 20:09:34 +0300

    finished geometrical calculator

---
+++ JavaWorkspace/src/main/java/com/example/javaworkspace/geometricalcalc/Shape.java
@@ -0,0 +1,13 @@
+package com.example.javaworkspace.geometricalcalc;
+
+public class Shape {
+    double area() {
+        return 0;
+    }
+    double perimeter(){
+        return 0;
+    }
+    String getName() {
+        return null;
+    }
+}
